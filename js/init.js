d1f1c4a30b0217598c368f67449e5bf8e431ea46
Aurelio Greco <aureliogreco@outlook.it>
2020-04-23 18:03:01 +0200

    Fixed titles with images -desktop and mobile-, created folder img.

--- js/init.js
+++ js/init.js
@@ -4,6 +4,6 @@
     $('.sidenav').sidenav({ edge: 'right' });
     $('.parallax').parallax();
     $('.carousel').carousel();
-    
+        
   }); // end of document ready
 })(jQuery); // end of jQuery name space
